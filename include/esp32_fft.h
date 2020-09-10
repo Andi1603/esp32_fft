6ec32647cee5bd91d26cf885cb0499ef4ddf9f37
Andreas Sch <andreas.schlager28@gmail.com>
2020-09-10 18:36:13 +0200

    Setup test environment

---
+++ include/esp32_fft.h
@@ -0,0 +1,8 @@
+#ifndef ESP32_FFT_H
+#define ESP32_FFT_H
+
+class esp32_fft {
+
+};
+
+#endif
---
+++ test/fft_properties_test.cpp
@@ -0,0 +1,7 @@
+
+#include <esp32_fft.h>
+#include <gtest/gtest.h>
+
+TEST(hallo, welt) {
+    esp32_fft fft();
+}
